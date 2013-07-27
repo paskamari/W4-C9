664c0d1f1696819ac3d7eeb89adcb06ff580f824
Hamid Paskamari <hpaskamari@gmail.com>
2013-07-27 13:49:16 +0430

    insert 1theme

---
+++ W4-1theme/content.php
@@ -0,0 +1,65 @@
+<div id="preloader-container">
+<div id="container">
+<?php 
+
+  $args = array(
+    'post_type' => 'portfolio',
+    'orderby' => 'name',
+    'order' => 'ASC'
+  );
+
+  $query = new WP_Query($args);
+
+
+  if($query->have_posts()){
+    while($query->have_posts()){
+      $class_name ='portfolio';
+      $query->the_post(); 
+      $types = get_the_terms($post->ID,'type');
+      if($types){
+        foreach($types as $type) {
+          $class_name .= " hp_".$type->slug;
+        }
+      }
+?>
+  <div class="widget <?php echo $class_name;?> web homepage">
+    <div class="entry-container span4">
+    
+      <!-- Portfolio Image -->
+      <div class="entry-image">
+        <a href="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>" class="fancybox">
+          <span class="entry-image-overlay"></span>
+          <?php the_post_thumbnail('medium'); ?>
+        </a>
+      </div>
+
+      <div class="entry drop-shadow curved ">
+
+        <!-- Portfolio Heading -->
+        <h5 class="heading">
+          <a href="portfolio-single.html">
+            <?php the_title(); ?>
+          </a>
+        </h5>
+        <div class="entry-footer">
+          <ul>
+            <li class="left">گرافیک</li>
+            <li>توسط :<?php the_author() ?></li>
+            <li class="right no-margin"><div class="icon like"></div> 3</li>
+      
+          </ul>
+        </div>
+
+        <p><?php the_excerpt(); ?></p>
+          
+        <div class="stripes"></div>
+      </div>      
+    </div>
+  </div>
+<?php
+  } //END WHILE
+  } //END IF
+  wp_reset_postdata();
+?>
+</div>
+</div>
